f4a2430ce18fb3fb5e1a9d8a0413dcf0ee114cd8
Dreaming-J <kimjjong321@naver.com>
2023-11-10 04:21:58 +0900

    :white_check_mark: test. 메뉴 카테고리 판단 테스트

--- src/test/java/christmas/model/MenuTest.java
+++ src/test/java/christmas/model/MenuTest.java
@@ -3,10 +3,13 @@ package christmas.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.exception.Exception.OrderException;
 import christmas.model.menu.Menu;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class MenuTest {
     @Test
@@ -15,6 +18,20 @@ public class MenuTest {
                 () -> assertThatThrownBy(() -> Menu.from("라멘"))
                         .isInstanceOf(OrderException.class),
                 () -> assertDoesNotThrow(() -> Menu.from("아이스크림"))
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value =
+            {"바비큐립,true,false,false",
+                    "초코케이크,false,true,false",
+                    "레드와인,false,false,true",
+                    "타파스,false,false,false"})
+    void 카테고리_판단_테스트(String food, boolean isMain, boolean isDessert, boolean isDrink) {
+        assertAll(
+                () -> assertEquals(Menu.from(food).isMain(), isMain),
+                () -> assertEquals(Menu.from(food).isDessert(), isDessert),
+                () -> assertEquals(Menu.from(food).isDrink(), isDrink)
         );
     }
 }
