f5b155ccee785b2bfa673d23c4e2a1a450c1178c
Dreaming-J <kimjjong321@naver.com>
2023-11-10 04:52:00 +0900

    :white_check_mark: test. 날짜 유효성 테스트

--- src/main/java/christmas/exception/Exception.java
+++ src/main/java/christmas/exception/Exception.java
@@ -6,4 +6,10 @@ public class Exception {
             super("유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
+
+    public static class DateException extends IllegalArgumentException {
+        public DateException() {
+            super("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
 }
---
+++ src/test/java/christmas/model/reservation/date/DateTest.java
@@ -0,0 +1,22 @@
+package christmas.model.reservation.date;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import christmas.exception.Exception.DateException;
+import org.junit.jupiter.api.Test;
+
+public class DateTest {
+    @Test
+    void 날짜_유효성_테스트() {
+        assertAll(
+                () -> assertThatThrownBy(() -> new Date(0))
+                        .isInstanceOf(DateException.class),
+                () -> assertThatThrownBy(() -> new Date(32))
+                        .isInstanceOf(DateException.class),
+                () -> assertDoesNotThrow(() -> new Date(1)),
+                () -> assertDoesNotThrow(() -> new Date(31))
+        );
+    }
+}
