8a9148c547cc3f3f6ada88aea5ca8c928bf9e76a
Dreaming-J <kimjjong321@naver.com>
2023-11-10 04:40:53 +0900

    :sparkles: feat. 요일 계산

--- src/main/java/christmas/model/reservation/Day.java
+++ src/main/java/christmas/model/reservation/Day.java
@@ -1,15 +1,26 @@
 package christmas.model.reservation;
 
+import java.util.stream.Stream;
+
 public enum Day {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
+    MONDAY(4),
+    TUESDAY(5),
+    WEDNESDAY(6),
+    THURSDAY(0),
+    FRIDAY(1),
+    SATURDAY(2),
+    SUNDAY(3);
+
+    private final int date;
+
+    Day(int date) {
+        this.date = date;
+    }
 
     public static Day from(int date) {
-        return null;
+        return Stream.of(values())
+                .filter(day -> day.date == date % 7)
+                .findFirst()
+                .get();
     }
 }
