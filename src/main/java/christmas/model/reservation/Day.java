8d4b3e9e67d26431ada5e97bfee1ac2c3242bbc0
Dreaming-J <kimjjong321@naver.com>
2023-11-10 04:36:01 +0900

    :white_check_mark: test. 요일 계산 테스트

---
+++ src/main/java/christmas/model/reservation/Day.java
@@ -0,0 +1,15 @@
+package christmas.model.reservation;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+    public static Day from(int date) {
+        return null;
+    }
+}
---
+++ src/test/java/christmas/model/reservation/DayTest.java
@@ -0,0 +1,28 @@
+package christmas.model.reservation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class DayTest {
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void 요일_계산_테스트(int num, Day day) {
+        assertEquals(Day.from(num), day);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(1, Day.FRIDAY),
+                Arguments.of(23, Day.SATURDAY),
+                Arguments.of(17, Day.SUNDAY),
+                Arguments.of(25, Day.MONDAY),
+                Arguments.of(5, Day.TUESDAY),
+                Arguments.of(20, Day.WEDNESDAY),
+                Arguments.of(7, Day.THURSDAY)
+        );
+    }
+}
