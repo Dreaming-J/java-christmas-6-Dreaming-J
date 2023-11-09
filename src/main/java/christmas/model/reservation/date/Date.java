4ab3ef08ca9c2ad467c31e6ac8baeba6330649ef
Dreaming-J <kimjjong321@naver.com>
2023-11-10 04:55:27 +0900

    :sparkles: feat. 날짜 유효성 검사

--- src/main/java/christmas/model/reservation/date/Date.java
+++ src/main/java/christmas/model/reservation/date/Date.java
@@ -1,9 +1,28 @@
 package christmas.model.reservation.date;
 
+import christmas.exception.Exception.DateException;
+
 public class Date {
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
     private final int date;
 
     public Date(int date) {
+        validate(date);
         this.date = date;
+    }
+
+    private void validate(int date) {
+        if (isUnderMin(date) || isOverMax(date)) {
+            throw new DateException();
+        }
+    }
+
+    private boolean isUnderMin(int date) {
+        return date < MIN_DATE;
+    }
+
+    private boolean isOverMax(int date) {
+        return date > MAX_DATE;
     }
 }
