7f140c2848012c2c8c317853607f506cbb6b8652
Dreaming-J <kimjjong321@naver.com>
2023-11-10 15:45:12 +0900

    :sparkles: feat. 이벤트 배지 부여

--- src/main/java/christmas/exception/Exception.java
+++ src/main/java/christmas/exception/Exception.java
@@ -4,6 +4,7 @@ public class Exception {
 
     private static final String ERROR_MSG_ORDER_EXCEPTION = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private static final String ERROR_MSG_DATE_EXCEPTION = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String ERROR_MSG_BADGE_EXCEPTION = "유효하지 않은 할인 금액입니다.";
 
     public static class OrderException extends IllegalArgumentException {
         public OrderException() {
@@ -14,6 +15,12 @@ public class Exception {
     public static class DateException extends IllegalArgumentException {
         public DateException() {
             super(ERROR_MSG_DATE_EXCEPTION);
+        }
+    }
+
+    public static class BadgeException extends IllegalArgumentException {
+        public BadgeException() {
+            super(ERROR_MSG_BADGE_EXCEPTION);
         }
     }
 }
--- src/main/java/christmas/model/reservation/benefit/Badge.java
+++ src/main/java/christmas/model/reservation/benefit/Badge.java
@@ -1,9 +1,35 @@
 package christmas.model.reservation.benefit;
 
+import christmas.exception.Exception.BadgeException;
+import java.util.stream.Stream;
+
 public enum Badge {
-    STAR, TREE, SANTA, NOTHING;
+    SANTA(20_000, "산타"),
+    TREE(10_000, "트리"),
+    STAR(5_000, "별"),
+    NOTHING(0, "없음");
+
+    private final int condition;
+    private final String name;
+
+    Badge(int condition, String name) {
+        this.condition = condition;
+        this.name = name;
+    }
 
     public static Badge from(int discount) {
-        return null;
+        return Stream.of(values())
+                .filter(badge -> badge.isMoreCondition(discount))
+                .findFirst()
+                .orElseThrow(BadgeException::new);
+    }
+
+    private boolean isMoreCondition(int discount) {
+        return discount >= this.condition;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
     }
 }
