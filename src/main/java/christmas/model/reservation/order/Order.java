823da9098f6ede042d8da37bfe095d1ed55a2dd0
Dreaming-J <kimjjong321@naver.com>
2023-11-10 14:33:30 +0900

    :recycle: refactor. orderMap 생성 객체 분리

--- src/main/java/christmas/model/reservation/order/Order.java
+++ src/main/java/christmas/model/reservation/order/Order.java
@@ -1,18 +1,12 @@
 package christmas.model.reservation.order;
 
-import static christmas.Config.MAX_TOTAL_QUANTITY;
 import static christmas.Config.ORDER_REGEX;
-import static christmas.util.Constant.COMMA;
-import static christmas.util.Constant.DASH;
+import static christmas.model.reservation.order.OrderMapGenerator.createOrderMap;
 
 import christmas.exception.Exception.OrderException;
 import christmas.model.menu.Menu;
-import christmas.util.TypeConverter;
-import java.util.EnumMap;
 import java.util.Map;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Order {
     private final Map<Menu, Quantity> order;
@@ -20,20 +14,6 @@ public class Order {
     public Order(String orders) {
         validateRegex(orders);
         this.order = createOrderMap(orders);
-    }
-
-    private Map<Menu, Quantity> createOrderMap(String orders) {
-        Map<Menu, Quantity> orderMap = Stream.of(orders.split(COMMA))
-                .map(order -> order.split(DASH))
-                .collect(Collectors
-                        .toMap(order -> Menu.from(order[0]),
-                                order -> new Quantity(TypeConverter.toInt(order[1])),
-                                (oldMenu, newMenu) -> {
-                                    throw new OrderException();
-                                },
-                                () -> new EnumMap<>(Menu.class)));
-        validateOrderMap(orderMap);
-        return orderMap;
     }
 
     private void validateRegex(String orders) {
@@ -44,23 +24,5 @@ public class Order {
 
     private boolean isNotRegex(String orders) {
         return !Pattern.matches(ORDER_REGEX, orders);
-    }
-
-    private void validateOrderMap(Map<Menu, Quantity> orderMap) {
-        Stream<Menu> orderMenus = orderMap.keySet().stream();
-        Stream<Quantity> orderQuantities = orderMap.values().stream();
-
-        if (isOnlyDrink(orderMenus) || isOverMaxTotalQuantity(orderQuantities)) {
-            throw new OrderException();
-        }
-    }
-
-    private boolean isOnlyDrink(Stream<Menu> orderMenus) {
-        return orderMenus.allMatch(Menu::isDrink);
-    }
-
-    private boolean isOverMaxTotalQuantity(Stream<Quantity> orderQuantities) {
-        return orderQuantities.mapToInt(Quantity::number)
-                .sum() > MAX_TOTAL_QUANTITY;
     }
 }
---
+++ src/main/java/christmas/model/reservation/order/OrderMapGenerator.java
@@ -0,0 +1,47 @@
+package christmas.model.reservation.order;
+
+import static christmas.Config.MAX_TOTAL_QUANTITY;
+import static christmas.util.Constant.COMMA;
+import static christmas.util.Constant.DASH;
+
+import christmas.exception.Exception.OrderException;
+import christmas.model.menu.Menu;
+import christmas.util.TypeConverter;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OrderMapGenerator {
+    public static Map<Menu, Quantity> createOrderMap(String orders) {
+        Map<Menu, Quantity> orderMap = Stream.of(orders.split(COMMA))
+                .map(order -> order.split(DASH))
+                .collect(Collectors
+                        .toMap(order -> Menu.from(order[0]),
+                                order -> new Quantity(TypeConverter.toInt(order[1])),
+                                (oldMenu, newMenu) -> {
+                                    throw new OrderException();
+                                },
+                                () -> new EnumMap<>(Menu.class)));
+        validateOrderMap(orderMap);
+        return orderMap;
+    }
+
+    private static void validateOrderMap(Map<Menu, Quantity> orderMap) {
+        Stream<Menu> orderMenus = orderMap.keySet().stream();
+        Stream<Quantity> orderQuantities = orderMap.values().stream();
+
+        if (isOnlyDrink(orderMenus) || isOverMaxTotalQuantity(orderQuantities)) {
+            throw new OrderException();
+        }
+    }
+
+    private static boolean isOnlyDrink(Stream<Menu> orderMenus) {
+        return orderMenus.allMatch(Menu::isDrink);
+    }
+
+    private static boolean isOverMaxTotalQuantity(Stream<Quantity> orderQuantities) {
+        return orderQuantities.mapToInt(Quantity::number)
+                .sum() > MAX_TOTAL_QUANTITY;
+    }
+}
