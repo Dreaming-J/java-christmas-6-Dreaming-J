0a826f67c01b250a5820f28a0c45d8c082400995
Dreaming-J <kimjjong321@naver.com>
2023-11-10 15:34:20 +0900

    :white_check_mark: test. 이벤트 배지 부여 테스트

---
+++ src/main/java/christmas/model/reservation/benefit/Badge.java
@@ -0,0 +1,9 @@
+package christmas.model.reservation.benefit;
+
+public enum Badge {
+    STAR, TREE, SANTA, NOTHING;
+
+    public static Badge from(int discount) {
+        return null;
+    }
+}
---
+++ src/test/java/christmas/model/reservation/benefit/BadgeTest.java
@@ -0,0 +1,25 @@
+package christmas.model.reservation.benefit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class BadgeTest {
+    @ParameterizedTest(name = "[{index}] 할인금액: {0}증정 배지: {1}")
+    @MethodSource("generateData")
+    void 이벤트_배지_부여_테스트(int discount, Badge badge) {
+        assertEquals(Badge.from(discount), badge);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(3_000, Badge.NOTHING),
+                Arguments.of(5_000, Badge.STAR),
+                Arguments.of(10_000, Badge.TREE),
+                Arguments.of(20_000, Badge.SANTA)
+        );
+    }
+}
