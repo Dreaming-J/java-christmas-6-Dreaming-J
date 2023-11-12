206a80a47e7d9f31ff2af7063524185a9d19a6fa
Dreaming-J <kimjjong321@naver.com>
2023-11-13 01:32:07 +0900

    :sparkles: feat. 이벤트 최소 주문금액

--- src/main/java/christmas/Config.java
+++ src/main/java/christmas/Config.java
@@ -2,6 +2,9 @@ package christmas;
 
 public class Config {
     public static final String ORDER_REGEX = "^([가-힣]+)-(\\d+)(,([가-힣]+)-(\\d+))*$";
+
     public static final int MIN_QUNTITY = 1;
     public static final int MAX_TOTAL_QUANTITY = 20;
+
+    public static final int MIN_AMOUNT_DUE_FOR_EVENT = 10_000;
 }
--- src/main/java/christmas/model/event/Event.java
+++ src/main/java/christmas/model/event/Event.java
@@ -1,5 +1,7 @@
 package christmas.model.event;
 
+import static christmas.Config.MIN_AMOUNT_DUE_FOR_EVENT;
+
 public abstract class Event {
     protected final int amountDue;
 
@@ -8,7 +10,7 @@ public abstract class Event {
     }
 
     public boolean isFitCondition() {
-        return false;
+        return this.amountDue >= MIN_AMOUNT_DUE_FOR_EVENT;
     }
 
     public abstract void applyBenefit();
