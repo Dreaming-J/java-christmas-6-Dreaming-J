0b2ddaed1fdbd016164bfd9fa85251bf3e919590
Dreaming-J <kimjjong321@naver.com>
2023-11-12 17:36:50 +0900

    :lipstick: style. Date class -> record 변경

--- src/main/java/christmas/model/reservation/date/Date.java
+++ src/main/java/christmas/model/reservation/date/Date.java
@@ -2,14 +2,12 @@ package christmas.model.reservation.date;
 
 import christmas.exception.Exception.DateException;
 
-public class Date {
+public record Date(int date) {
     private static final int MIN_DATE = 1;
     private static final int MAX_DATE = 31;
-    private final int date;
 
-    public Date(int date) {
+    public Date {
         validate(date);
-        this.date = date;
     }
 
     private void validate(int date) {
