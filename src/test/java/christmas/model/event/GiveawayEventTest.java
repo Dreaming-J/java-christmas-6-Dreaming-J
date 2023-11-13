fd53a335da9dc0b4a73d3c42b3e7ed8152a00f69
Dreaming-J <kimjjong321@naver.com>
2023-11-13 16:31:49 +0900

    :white_check_mark: test. 증정_이벤트 적용 테스트

--- src/test/java/christmas/model/event/GiveawayEventTest.java
+++ src/test/java/christmas/model/event/GiveawayEventTest.java
@@ -3,8 +3,11 @@ package christmas.model.event;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.model.date.Date;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class GiveawayEventTest {
     @ParameterizedTest(name = "[{index}] 총주문금액:{0}, 결과:{1}")
@@ -12,5 +15,20 @@ public class GiveawayEventTest {
     void 조건_테스트(int amountDue, boolean isFit) {
         Event giveawayEvent = new GiveawayEvent(new Date(1), amountDue);
         assertEquals(giveawayEvent.canDiscount(), isFit);
+    }
+
+    @ParameterizedTest(name = "[{index}] 총주문금액:{0}, 결과:{1}")
+    @MethodSource("generateData")
+    void 할인_적용_테스트(int amountDue, String discount) {
+        Event giveawayEvent = new GiveawayEvent(new Date(1), amountDue);
+        giveawayEvent.applyBenefit(0);
+        assertEquals(giveawayEvent.toString(), discount);
+    }
+
+    private static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(119_999, ""),
+                Arguments.of(120_000, "증정 이벤트: -25,000원")
+        );
     }
 }
