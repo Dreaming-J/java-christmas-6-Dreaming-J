34d01861abad363427bfcc7ebd8424d43b1e792f
Dreaming-J <kimjjong321@naver.com>
2023-11-13 15:10:40 +0900

    :sparkles: feat. 주말_할인 적용

--- src/main/java/christmas/model/event/WeekendEvent.java
+++ src/main/java/christmas/model/event/WeekendEvent.java
@@ -1,5 +1,10 @@
 package christmas.model.event;
 
+import static christmas.config.EventConfig.COMMA_FORMATTER;
+import static christmas.config.EventConfig.WEEK_EVENT_DISCOUNT_UNIT;
+import static christmas.model.event.EventMsg.WEEKEND_EVENT_MSG;
+import static christmas.util.Constant.EMPTY;
+
 import christmas.model.reservation.date.Date;
 
 public class WeekendEvent extends Event {
@@ -13,7 +18,20 @@ public class WeekendEvent extends Event {
     }
 
     @Override
-    public void applyBenefit(int applicableTarget) {
+    public void applyBenefit(int quantityOfMains) {
+        if (!canDiscount()) {
+            return;
+        }
 
+        this.discount = quantityOfMains * WEEK_EVENT_DISCOUNT_UNIT;
+    }
+
+    @Override
+    public String toString() {
+        if (doesNotExistDiscount()) {
+            return EMPTY;
+        }
+
+        return String.format(WEEKEND_EVENT_MSG.toString(), COMMA_FORMATTER.format(this.discount));
     }
 }
