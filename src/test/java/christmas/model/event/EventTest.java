f48fe0105eba5fb6ece9cf33e0c8e315470e2e2d
Dreaming-J <kimjjong321@naver.com>
2023-11-13 13:05:04 +0900

    :white_check_mark: test. 크리스마스_디데이_할인 적용 테스트

--- src/test/java/christmas/model/event/EventTest.java
+++ src/test/java/christmas/model/event/EventTest.java
@@ -3,10 +3,13 @@ package christmas.model.event;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.model.reservation.date.Date;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class EventTest {
     @ParameterizedTest
@@ -28,6 +31,22 @@ public class EventTest {
         void 조건_테스트(int amountDue, int date, boolean isFit) {
             Event christmasDdayEvent = new ChristmasDdayEvent(amountDue, new Date(date));
             assertEquals(christmasDdayEvent.isFitCondition(), isFit);
+        }
+
+        @ParameterizedTest(name = "[{index}] 날짜:{1}, 결과:{2}")
+        @MethodSource("generateData")
+        void 할인_적용_테스트(int amountDue, int date, String discount) {
+            Event christmasDdayEvent = new ChristmasDdayEvent(amountDue, new Date(date));
+            christmasDdayEvent.applyBenefit();
+            assertEquals(christmasDdayEvent.toString(), discount);
+        }
+
+        private static Stream<Arguments> generateData() {
+            return Stream.of(
+                    Arguments.of(10_000, 1, "크리스마스 디데이 할인: -1,000원"),
+                    Arguments.of(10_000, 25, "크리스마스 디데이 할인: -3,400원"),
+                    Arguments.of(10_000, 30, "")
+            );
         }
     }
 }
