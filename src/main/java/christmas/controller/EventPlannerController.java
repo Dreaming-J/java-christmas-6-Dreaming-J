e0b0b90d6ee92215b9497d11fabdb0f2a7944c62
Dreaming-J <kimjjong321@naver.com>
2023-11-14 15:14:48 +0900

    :sparkles: feat. 방문 날짜 입력 프로세스

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,15 +1,28 @@
 package christmas.controller;
 
+import christmas.model.date.Date;
+import christmas.module.RepeatModule;
+import christmas.view.InputView;
 import christmas.view.OutputView;
 
-public class EventPlannerController {
+public class EventPlannerController extends RepeatModule {
+    private final InputView inputView;
     private final OutputView outputView;
 
     public EventPlannerController() {
+        inputView = new InputView();
         outputView = new OutputView();
     }
 
     public void start() {
         outputView.printStartPlanner();
+        reserve();
+    }
+
+    private void reserve() {
+        Date reserveDate = repeat(() -> {
+            int date = inputView.readDate();
+            return new Date(date);
+        });
     }
 }
