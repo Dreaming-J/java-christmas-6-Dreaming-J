273bbec7ee06527414fcec504277ea4697990d28
Dreaming-J <kimjjong321@naver.com>
2023-11-14 16:26:19 +0900

    :recycle: refacotor. 사용하지 않은 메서드 삭제

--- src/main/java/christmas/dto/Reservation.java
+++ src/main/java/christmas/dto/Reservation.java
@@ -4,11 +4,5 @@ import christmas.model.date.Date;
 import christmas.model.order.Order;
 
 public record Reservation(Date date, Order order) {
-    public String toStringByDate() {
-        return date.toString();
-    }
 
-    public String toStringByOrder() {
-        return order.toString();
-    }
 }
