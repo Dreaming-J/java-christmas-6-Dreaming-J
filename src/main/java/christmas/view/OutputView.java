a461323f2a77e1871c4196ab110ed7a91744a853
Dreaming-J <kimjjong321@naver.com>
2023-11-14 15:31:56 +0900

    :sparkles: feat. 방문 날짜 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,16 @@
 package christmas.view;
 
+import static christmas.util.Constant.LINE_BREAK;
 import static christmas.view.Message.PRINT_START_PLANNER_MSG;
+
+import christmas.model.date.Date;
 
 public class OutputView {
     public void printStartPlanner() {
         System.out.println(PRINT_START_PLANNER_MSG);
+    }
+
+    public void printDate(Date date) {
+        System.out.println(date + LINE_BREAK);
     }
 }
