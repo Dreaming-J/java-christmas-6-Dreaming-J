bd2cb2d632efc1fb4f35d542b751bea363954229
Dreaming-J <kimjjong321@naver.com>
2023-11-14 15:16:49 +0900

    :sparkles: feat. 주문 메뉴와 개수 입력

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,6 +2,7 @@ package christmas.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static christmas.view.Message.READ_DATE_MSG;
+import static christmas.view.Message.READ_ORDER_MSG;
 
 import christmas.util.TypeConverter;
 
@@ -9,5 +10,10 @@ public class InputView {
     public int readDate() {
         System.out.println(READ_DATE_MSG);
         return TypeConverter.toIntForDate(readLine());
+    }
+
+    public String readOrder() {
+        System.out.println(READ_ORDER_MSG);
+        return readLine();
     }
 }
