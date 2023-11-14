923b47fcea98f0c17feffb834b48201d2aa8a639
Dreaming-J <kimjjong321@naver.com>
2023-11-14 15:36:37 +0900

    :sparkles: feat. 할인 전 총주문 금액 출력

--- src/main/java/christmas/view/Message.java
+++ src/main/java/christmas/view/Message.java
@@ -4,7 +4,8 @@ public enum Message {
     READ_DATE_MSG("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     READ_ORDER_MSG("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
     PRINT_START_PLANNER_MSG("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
-    PRINT_ORDER_MENU_TITLE("<주문 메뉴>");
+    PRINT_ORDER_MENU_TITLE("<주문 메뉴>"),
+    PRINT_AMOUNT_DUE_TITLE("<할인 전 총주문 금액>");
 
     private final String message;
 
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,11 @@
 package christmas.view;
 
 import static christmas.util.Constant.LINE_BREAK;
+import static christmas.view.Message.PRINT_AMOUNT_DUE_TITLE;
 import static christmas.view.Message.PRINT_ORDER_MENU_TITLE;
 import static christmas.view.Message.PRINT_START_PLANNER_MSG;
 
+import christmas.model.Money;
 import christmas.model.date.Date;
 import christmas.model.order.Order;
 
@@ -18,6 +20,11 @@ public class OutputView {
 
     public void printOrder(Order order) {
         System.out.println(PRINT_ORDER_MENU_TITLE);
-        System.out.println(order);
+        System.out.println(order + LINE_BREAK);
+    }
+
+    public void printAmountDue(Money amountDue) {
+        System.out.println(PRINT_AMOUNT_DUE_TITLE);
+        System.out.println(amountDue);
     }
 }
