d5c9c0314ae6f21fd04c1849ea3a5a8d456c055e
Dreaming-J <kimjjong321@naver.com>
2023-11-14 15:34:06 +0900

    :sparkles: feat. 주문 메뉴 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,11 @@
 package christmas.view;
 
 import static christmas.util.Constant.LINE_BREAK;
+import static christmas.view.Message.PRINT_ORDER_MENU_TITLE;
 import static christmas.view.Message.PRINT_START_PLANNER_MSG;
 
 import christmas.model.date.Date;
+import christmas.model.order.Order;
 
 public class OutputView {
     public void printStartPlanner() {
@@ -12,5 +14,10 @@ public class OutputView {
 
     public void printDate(Date date) {
         System.out.println(date + LINE_BREAK);
+    }
+
+    public void printOrder(Order order) {
+        System.out.println(PRINT_ORDER_MENU_TITLE);
+        System.out.println(order);
     }
 }
